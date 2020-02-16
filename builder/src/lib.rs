47a8442757fd22657ed42160ca67448eac8a8ff1
elycruz <603428+elycruz@users.noreply.github.com>
2020-02-15 22:05:36 -0500

    builder - partial solution to test-suite '08...'.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -3,12 +3,16 @@ extern crate syn;
 
 use syn::{parse_macro_input, DeriveInput};
 use quote::quote;
-use proc_macro2::{Span, TokenStream, Literal, Ident, TokenTree};
+use proc_macro2::{Span, TokenStream, Ident, TokenTree};
+use syn::spanned::Spanned;
 
 const MALFORMED_ATTR_MSG: &str = "Malformed builder attribute; Expected format `each = \"method-name-to-create\"`;";
 
 fn is_option_type_path(tp: &syn::TypePath) -> bool {
-    tp.path.segments.len() > 0 && tp.path.segments[0].ident == "Option" // @todo check for std::option::Option here
+    tp.path.segments.len() > 0 && (
+        tp.path.segments[0].ident == "Option" ||
+            tp.path.segments[0].ident == "std::option::Option"
+    )
 }
 
 fn is_type_path_match(pred: impl Fn(&syn::TypePath) -> bool, ty: &syn::Type) -> bool {
@@ -18,35 +22,39 @@ fn is_type_path_match(pred: impl Fn(&syn::TypePath) -> bool, ty: &syn::Type) -> 
     }
 }
 
-fn always_true(tp: &syn::TypePath) -> bool {
+fn always_true_on_tp(tp: &syn::TypePath) -> bool {
     true
 }
 
-fn unwrap_ty_on_tp(pred: impl Fn(&syn::TypePath) -> bool, ty: &syn::Type) -> &syn::Type {
+fn unwrap_ty_on_tp(pred: impl Fn(&syn::TypePath) -> bool, ty: &syn::Type) -> Result<&syn::Type, &'static str> {
     match ty {
         syn::Type::Path(tp) => {
             if !pred(tp) {
-                return ty;
+                return Ok(ty);
             }
             match &tp.path.segments[0].arguments {
                 syn::PathArguments::AngleBracketed(pa) =>
                     match &pa.args[0] {
-                        syn::GenericArgument::Type(t) => t,
-                        _ => panic!("malformed `U<V>` encountered")
+                        syn::GenericArgument::Type(t) => Ok(t),
+                        _ => Err("malformed `U<V>` encountered")
                     },
                 _ => unreachable!()
             }
         }
-        _ => ty
+        _ => Ok(ty)
     }
 }
 
-fn resolve_attrs_methods(f: &syn::Field, method_quotes: &mut Vec<TokenStream>) {
+fn parse_attrs(f: &syn::Field, method_quotes: &mut Vec<TokenStream>) -> Result<Option<Ident>, syn::Error> {
     let attrs = &f.attrs;
-    let name = &f.ident;
+    let name = match &f.ident {
+        Some(i) => i.clone(),
+        _ => unreachable!()
+    };
+    let mut attr_name_ident = None;
     let ty = &f.ty;
     if attrs.len() == 0 {
-        return;
+        return Ok(None);
     }
     for attr in attrs {
         // Only match 'outer' attribute types
@@ -61,53 +69,49 @@ fn resolve_attrs_methods(f: &syn::Field, method_quotes: &mut Vec<TokenStream>) {
                 continue;
             }
             if attr.tokens.is_empty() {
-                panic!("The `builder` attribute requires a group value;  E.g., `each = \"method-name\"`;")
+                return Err(syn::Error::new(ps.ident.span(), "expected `builder(each = \"...\")`"));
             }
         }
 
         // Get the `Group` value
         if let Some(proc_macro2::TokenTree::Group(g)) = attr.tokens.clone().into_iter().next() {
-            eprintln!("Group extraction: {:#?}", g);
-
             let mut ts = g.clone().stream().into_iter();
-            eprintln!("Token stream: {:#?}", &ts);
 
             let each_ident = match ts.next() {
                 Some(tt) => if &tt.to_string() != "each" {
-                    panic!(MALFORMED_ATTR_MSG);
+                    return Err(syn::Error::new(tt.span(), "expected `builder(each = \"...\")`"));
                 } else {
                     tt
                 },
-                _ => panic!(MALFORMED_ATTR_MSG)
+                _ => return Err(syn::Error::new(Span::call_site(), "expected `builder(each = \"...\")`"))
             };
 
             let eq_punct = match ts.next() {
                 Some(tt) =>
                     if &tt.to_string() != "=" {
-                        panic!(MALFORMED_ATTR_MSG);
+                        return Err(syn::Error::new(tt.span(), "expected `builder(each = \"...\")`"));
                     } else {
                         tt
                     },
-                _ => panic!(MALFORMED_ATTR_MSG)
+                _ => return Err(syn::Error::new(Span::call_site(), "expected `builder(each = \"...\")`"))
             };
 
+            // If we 'method_name' literal convert and use it
             if let Some(TokenTree::Literal(fn_name_lit)) = ts.next() {
-                // eprintln!("Expected literal: {:#?}", fn_name_lit);
                 let fn_name = match syn::Lit::new(fn_name_lit) {
                     syn::Lit::Str(ls) => ls.value(),
-                    _ => panic!(MALFORMED_ATTR_MSG)
+                    _ => return Err(syn::Error::new(Span::call_site(), "expected `builder(each = \"...\")`"))
                 };
                 let ident = Ident::new(&fn_name, Span::call_site());
-                let inner_ty = unwrap_ty_on_tp(always_true, ty);
-                match &name {
-                    Some(i) => {
-                        // if field and requested method name are the same, bail
-                        if ident.eq(i) {
-                            continue;
-                        }
-                    }
-                    _ => unreachable!()
+                let inner_ty = match unwrap_ty_on_tp(always_true_on_tp, ty) {
+                    Ok(_type) => _type,
+                    _ => return Err(syn::Error::new(Span::call_site(), "expected `builder(each = \"...\")`"))
+                };
+                // if field and requested method name are the same, bail
+                if ident.eq(&name) {
+                    continue;
                 }
+                attr_name_ident = Some(name.clone());
                 method_quotes.push(quote! {
                     pub fn #ident (&mut self, x: #inner_ty) -> &mut Self {
                         match &mut self.#name {
@@ -122,30 +126,36 @@ fn resolve_attrs_methods(f: &syn::Field, method_quotes: &mut Vec<TokenStream>) {
                     }
                 });
             } else {
-                panic!(MALFORMED_ATTR_MSG);
+                return Err(syn::Error::new(Span::call_site(), "expected `builder(each = \"...\")`"));
             }
         } else {
-            panic!(MALFORMED_ATTR_MSG);
+            return Err(syn::Error::new(Span::call_site(), "expected `builder(each = \"...\")`"));
         }
     }
+    Ok(attr_name_ident)
 }
 
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let _ast = parse_macro_input!(input as DeriveInput);
-    let (_bfields, _bmethods, _bextracts, _bdefaults) = match &_ast.data {
+    let (_bfields, _bmethods, _bextracts) = match &_ast.data {
         syn::Data::Enum(_) => panic!("`Builder` can only be derived for `structs`."),
         syn::Data::Union(_) => panic!("`Builder` can only be derived for `structs`."),
         syn::Data::Struct(element) => {
             let mut _fields: Vec<TokenStream> = vec![];
             let mut _methods: Vec<TokenStream> = vec![];
             let mut _extracts: Vec<TokenStream> = vec![];
-            let mut _defaults: Vec<TokenStream> = vec![];
             for field in element.fields.iter() {
-                let name = &field.ident;
+                let name = match &field.ident {
+                    Some(i) => i,
+                    _ => unreachable!()
+                };
                 let ty = &field.ty;
-                let resolved_ty = unwrap_ty_on_tp(is_option_type_path, ty);
-                let mut is_option_tp: bool = is_type_path_match(is_option_type_path, ty);
+                let resolved_ty = match unwrap_ty_on_tp(is_option_type_path, ty) {
+                    Ok(_type) => _type,
+                    _ => unreachable!()
+                };
+                let is_option_tp: bool = is_type_path_match(is_option_type_path, ty);
                 _fields.push(if is_option_tp {
                     quote! {
                         #name: #ty
@@ -155,27 +165,39 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                         #name: std::option::Option<#ty>
                     }
                 });
-                resolve_attrs_methods(field, &mut _methods);
-                _methods.push(quote! {
-                    pub fn #name (&mut self, x: #resolved_ty) -> &mut Self {
-                        self.#name = Some(x);
-                        self
+                let attrs_parse_result: Result<(), syn::Error> = match parse_attrs(field, &mut _methods) {
+                    Ok(attr_meth_name) => {
+                        if attr_meth_name.is_none() || !(&attr_meth_name.unwrap()).eq(name) {
+                            _methods.push(quote! {
+                                pub fn #name (&mut self, x: #resolved_ty) -> &mut Self {
+                                    self.#name = Some(x);
+                                    self
+                                }
+                            });
+                        }
+                        _extracts.push(if is_option_tp {
+                            quote! {
+                                #name: self.#name.clone()
+                            }
+                        } else {
+                            quote! {
+                                    #name: if self.#name.is_none() {
+                                        std::default::Default::default()
+                                    } else {
+                                        self.#name.clone().unwrap()
+                                    }
+                                }
+                        });
+                        Ok(())
                     }
-                });
-                _extracts.push(if is_option_tp {
-                    quote! {
-                        #name: self.#name.clone()
-                    }
-                } else {
-                    quote! {
-                        #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is required.")).unwrap()
-                    }
-                });
-                _defaults.push(quote! {
-                    #name: None
-                });
+                    Err(e) => Err(e)
+                };
+
+                if let Err(e) = attrs_parse_result {
+                    return e.to_compile_error().into();
+                }
             }
-            (_fields, _methods, _extracts, _defaults)
+            (_fields, _methods, _extracts)
         }
     };
     let _nident = &_ast.ident;
@@ -199,9 +221,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
         impl #_nident {
             fn builder () -> #_bident {
-                #_bident {
-                    #(#_bdefaults,)*
-                }
+                std::default::Default::default()
             }
         }
     };
