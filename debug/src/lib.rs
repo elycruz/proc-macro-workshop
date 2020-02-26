cb2fd6d197413f212f63c4af6bfefdc3ef0f08e2
Ely De La Cruz <elycruz@elycruz.com>
2020-02-26 01:09:40 -0500

    flattened current progress.

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -8,29 +8,26 @@ use syn::{self, parse_macro_input, DeriveInput};
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ast = parse_macro_input!(input as DeriveInput);
     let _struct_ident = &_ast.ident;
-
-    match &_ast.data {
+    let _element = match &_ast.data {
         syn::Data::Enum(_) => panic!("`Builder` can only be derived for `structs`."),
         syn::Data::Union(_) => panic!("`Builder` can only be derived for `structs`."),
-        syn::Data::Struct(_element) => {
-            eprintln!("{:#?}", _ast.ident);
+        syn::Data::Struct(_element) => _element
+    };
+    let _fields_iter = match &_element.fields {
+        syn::Fields::Unnamed(_) => {
+            panic!("Only named fields allowed for structs")
+        }
+        syn::Fields::Unit => {
+            panic!("Only named fields allowed for structs")
+        }
+        syn::Fields::Named(syn::FieldsNamed{named, ..}) => named.iter()
+    };
 
-            match &_element.fields {
-                syn::Fields::Unnamed(_) => {
-                    panic!("Only named fields allowed for structs")
-                }
-                syn::Fields::Unit => {
-                    panic!("Only named fields allowed for structs")
-                }
-                syn::Fields::Named(fields) => {
-                    let fields =  fields.named.iter();
-                    for field in fields {
-                        eprintln!("{:#?}", &field.ident);
-                    }
-                }
-            } // fields match
-        } // data struct match
-    }
+    // eprintln!("{:#?}", _ast.ident);
+
+    /*for field in _fields_iter {
+        eprintln!("{:#?}", &field.ident);
+    }*/
 
     let _expanded = quote! {
         impl std::fmt::Debug for #_struct_ident {
