f897ecd2e0b530b86f1f7d50a8853b34b38d64ce
elycruz <603428+elycruz@users.noreply.github.com>
2020-03-01 15:14:16 -0500

    backup on test-05-06.

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,8 +2,10 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::*;
-use syn::{self, parse_macro_input, DeriveInput};
+use syn::{self, parse_macro_input, parse_quote, DeriveInput};
 use syn::export::TokenStream2;
+use syn::token::Token;
+use std::any::Any;
 
 fn extract_debug_name_value_pair(_field: &syn::Field) -> Result<Option<syn::MetaNameValue>, syn::Error> {
     for attr in &_field.attrs {
@@ -25,6 +27,28 @@ fn extract_debug_name_value_pair(_field: &syn::Field) -> Result<Option<syn::Meta
     Ok(None)
 }
 
+// Add a bound `T: HeapSize` to every type parameter T.
+fn add_trait_bounds(_add_debug_bound: bool, mut generics: syn::Generics) -> syn::Generics {
+    for param in &mut generics.params {
+        if let syn::GenericParam::Type(ref mut type_param) = *param {
+            // @todo check for `PhantomData...` here, if found, don't push `Debug` bound, else
+            type_param.bounds.push(parse_quote!(std::fmt::Debug));
+        }
+    }
+    generics
+}
+
+fn is_ty_not_phantom_data(typ: &syn::TypePath) -> bool {
+    for ps in &typ.path.segments {
+        if ps.ident.to_string().as_str().eq("PhantomData") {
+            if let syn::PathArguments::AngleBracketed(_generic_args) = &ps.arguments {
+                for gen_arg in &_generic_args.args.into_iter() {}
+            }
+        }
+    }
+    false
+}
+
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ast = parse_macro_input!(input as DeriveInput);
@@ -43,10 +67,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
         syn::Fields::Named(syn::FieldsNamed { named, .. }) => named.iter()
     };
-
     let mut _fmt_parts: Vec<String> = vec![];
     let mut _extract_parts: Vec<TokenStream2> = vec![];
+    let mut _generic_ty_in_phantom_data: bool = false;
     for _field in _fields_iter.clone() {
+        // if let sy
+        // is_ty_not_phantom_data(&_field.ty);
         let _field_ident = match &_field.ident {
             Some(_field_ident) => _field_ident,
             _ => continue
@@ -74,8 +100,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
         ]
             .join(" "));
 
+    let _generics = add_trait_bounds(_generic_ty_in_phantom_data, _ast.generics);
+    let (impl_generics, ty_generics, where_clause) = &_generics.split_for_impl();
     let _expanded = quote! {
-        impl std::fmt::Debug for #_struct_ident {
+        impl #impl_generics std::fmt::Debug for #_struct_ident #ty_generics #where_clause {
           fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             println!(#_fmt_str, #(#_extract_parts,)*);
             write!(f, #_fmt_str, #(#_extract_parts,)*)
